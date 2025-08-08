7f1751ca1acfdebbd47a7786212adfe43a45b455
Alex Zhou <alex.zzh.s7@gmail.com>
2025-08-08 14:01:38 +0900

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,23 @@
-export const Config = {
+interface ConfigType {
+  header: {
+    resize: boolean,
+    line: boolean,
+    background: 'transparent' | 'white' | 'blur'
+  },
+  text: {
+    horizontalMargin: string,
+    verticalMargin: string,
+  }
+};
 
-
+export const Config : ConfigType = {
+  header: {
+    resize: true,
+    line: false,
+    background: 'blur'
+  },
+  text: {
+    horizontalMargin: '2rem',
+    verticalMargin: '4rem'
+  }
 };
