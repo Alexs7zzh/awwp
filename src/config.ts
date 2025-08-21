cebc4c898e6d2c203fbf5b8f1d15dc638366061d
Alex Zhou <alex.zzh.s7@gmail.com>
2025-08-21 17:11:11 +0900

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,7 @@
 interface ConfigType {
-  header: {
-    resize: boolean;
-    line: boolean;
-    background: "transparent" | "white" | "blur";
-  };
   description: {
     horizontalMargin: string;
     verticalMargin: string;
-  };
-  textBlock: {
-    style: "wide" | "hero";
-  };
-  background: {
-    theme: "light" | "dark";
   };
   detailPage: {
     theme: 1 | 2 | 3;
@@ -24,20 +13,9 @@ interface ConfigType {
 }
 
 export const Config: ConfigType = {
-  header: {
-    resize: false,
-    line: false,
-    background: "transparent",
-  },
   description: {
     horizontalMargin: "2rem",
     verticalMargin: "2rem",
-  },
-  textBlock: {
-    style: "hero",
-  },
-  background: {
-    theme: "dark",
   },
   detailPage: {
     theme: 3,
