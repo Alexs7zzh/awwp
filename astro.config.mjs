101066c735acea643b805047f2126ab3351482c6
Alex Zhou <alex.zzh.s7@gmail.com>
2025-08-25 13:27:47 +0900

    Cleanup packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   prefetch: {
     defaultStrategy: "viewport",
   },
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap()],
 });
